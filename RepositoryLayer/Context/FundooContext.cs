c019b8181ab67f682d0dd035c5011e02fdb99cbf
pravinc3798 <pravin.choudhary88@gmail.com>
2022-11-02 17:43:10 +0530

    Added FundooContext, added required packages, added connection string in appsettings, added services.addbcontext

---
+++ RepositoryLayer/Context/FundooContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryLayer.Entity;
+
+namespace RepositoryLayer.Context
+{
+    public class FundooContext : DbContext
+    {
+        //Added Microsoft.Entityframeworkcore 3.1.0
+        //Added Microsoft.Entityframeworkcore.Relation 3.1.0
+        //Added Microsoft.Entityframeworkcore.SqlServer 3.1.0
+        //Added Microsoft.Entityframeworkcore.Tools 3.1.0
+        //Added Microsoft.EntityFrameworkCore.Design 3.1.0
+        //services.AddDbContext<FundooContext>(opts => opts.UseSqlServer(Configuration["ConnectionString:FundooDB"])); - Startup.configureservices
+        //Add-Migration 
+        public FundooContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<UserEntity> UserTable { get; set; }
+    }
+}
